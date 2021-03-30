16913f09a31a596c859495a4319240b854e3c88d
Anderson Barros Silva <anderson.tec12@gmail.com>
2021-03-29 23:40:06 -0300

    add auth with github

--- src/components/SignButton/index.tsx
+++ src/components/SignButton/index.tsx
@@ -1,15 +1,20 @@
 import styles from './styles.module.scss'
 import {FaGithub} from 'react-icons/fa'
+import {signIn, signOut,useSession} from 'next-auth/client'
+
 import {FiX} from 'react-icons/fi'
 export function SignButton(){
-  const isUSerLoggedIn = true
+  const [session] = useSession()
+  const isUSerLoggedIn = session
 
+  console.log(session)
   return (
 
     !isUSerLoggedIn ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => {signIn('github')}}
     >
       <FaGithub  color="#eba417" />
       
@@ -18,10 +23,11 @@ export function SignButton(){
       <button
       type="button"
         className={styles.signInButton}
+        onClick={() => {signOut()}}
       >
         
         <FaGithub  color="#04d361" />
-        Anderson Barros Silva 
+        {session.user.name}
         <FiX color="#737380" className={styles.closeIcon}/>
       </button>
     )
