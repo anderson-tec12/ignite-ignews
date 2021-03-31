cfb7b2a8e3dad22e8e1d75049c6dc7da11657ebe
Anderson Barros Silva <anderson.tec12@gmail.com>
2021-03-31 09:11:45 -0300

    ajusts

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -22,13 +22,33 @@ export default NextAuth({
         const {email} = user 
 
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            {
-              data:{email}
-            }
-          )
+         
+            q.If(
+              q.Not(
+                q.Exists(
+                  q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(user.email)
+                  )
+                )
+              ),
+              q.Create(
+                q.Collection('users'),
+                {
+                  data:{email}
+                }
+              ),
+              q.Get(
+                q.Exists(
+                  q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(user.email)
+                  )
+                )
+              )
+            )            
         )
+        
 
         return true
       }catch(err){
