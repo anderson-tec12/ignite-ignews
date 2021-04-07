43cb4c93d56c67918cb925b0f88480fc7bcff1dd
Anderson Barros Silva <anderson.tec12@gmail.com>
2021-04-07 08:37:16 -0300

    prismic api

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,26 @@
 import { SignButton } from '../SignButton'
+import Link from 'next/link'
 import styles from './styles.module.scss'
+import { useRouter } from 'next/router'
 
 export function Header(){
+  const {asPath} = useRouter()
+  
   return(
     <header className={styles.headerContainer}>
     <div className={styles.headerContent}>
       <img src="/images/logo.svg" alt="ig.news" />
 
       <nav>
-        <a href="" className={styles.active}>Home</a>
-        <a href="">Posts</a>
+        <Link href="/">
+          <a  className={styles.active}>Home</a>
+        </Link>
+
+        <Link href="/posts" prefetch>
+          <a >Posts</a>
+        </Link>
+        
+        
       </nav>
 
       <SignButton />
