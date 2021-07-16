59606454db8bb03782d58a58a72c6703d6dc23d4
Anderson Barros Silva <anderson.tec12@gmail.com>
2021-07-16 06:27:07 -0300

    test signButton component

---
+++ src/components/SignButton/SignButton.spec.tsx
@@ -0,0 +1,40 @@
+/**
+ * @jest-environment jsdom
+ */
+import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom";
+import "identity-obj-proxy";
+
+import { render, screen } from "@testing-library/react";
+import { SignButton } from ".";
+import { useSession } from "next-auth/client";
+import { mocked } from "ts-jest/utils";
+
+jest.mock("next-auth/client");
+
+describe("SignButton Component", () => {
+  it("renders correctly when user is not authenticated", () => {
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce([null, false]);
+    render(<SignButton />);
+
+    expect(screen.getByText("Sign in with Github")).toBeInTheDocument();
+  });
+
+  it("renders correctly when user is authenticated", () => {
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce([
+      {
+        user: { name: "John Doe", email: "johndoe@exemple.com" },
+        expires: "fake-expires",
+        activeSubscription: null,
+      },
+      false,
+    ]);
+    render(<SignButton />);
+
+    expect(screen.getByText("John Doe")).toBeInTheDocument();
+  });
+});
